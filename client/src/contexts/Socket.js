3a44c82f50ef2653cbecb503051a6f46aa3b212e
Veeravajhula Sai Mathur <51848442+vsaimathur@users.noreply.github.com>
2021-04-29 21:29:07 +0530

    modifying json files and some env variables to deploy the application

--- client/src/contexts/Socket.js
+++ client/src/contexts/Socket.js
@@ -2,5 +2,5 @@ import React from "react";
 import {io} from "socket.io-client";
 
 //Socket Initialization (*** transports are very necessary, without that sockets are not working)
-export const socket = io("http://127.0.0.1:5000", {transports: ['websocket', 'polling', 'flashsocket']});
+export const socket = io(process.env.SERVER_URL || "http://127.0.0.1:5000", {transports: ['websocket', 'polling', 'flashsocket']});
 export const SocketContext = React.createContext();
